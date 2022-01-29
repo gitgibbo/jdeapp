693e0d91fc6d887ac87c7acec3854ef189c8148c
Gibbo <mattgibbons@me.com>
2022-01-29 12:43:36 +0000

    changes to admin stuff

--- bos/admin.py
+++ bos/admin.py
@@ -5,3 +5,8 @@ admin.site.register(BOS)
 admin.site.register(BOS_topic)
 admin.site.register(BOS_shift)
 admin.site.register(BOS_area)
+
+
+admin.site.site_header = "JDE application Admin"
+admin.site.site_title = "JDE App"
+admin.site.index_title = "Welcome to JDE App Portal"
--- jde_app/settings.py
+++ jde_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-atjxiqe&n_gk&(!-68lb^$h$o*g)xi#31i$rr@4nd8q*ld2kvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
