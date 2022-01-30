975e0147cdbc6032ef864c59965b451c5ba197a9
Gibbo <mattgibbons@me.com>
2022-01-30 00:10:15 +0000

    Changed permit to work verbose name

--- permit_to_work/apps.py
+++ permit_to_work/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PermitToWorkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'permit_to_work'
+    verbose_name = 'Permit to work'
--- permit_to_work/models.py
+++ permit_to_work/models.py
@@ -4,9 +4,10 @@ from bos.models import Area
 class permit_to_work(models.Model):
     """Basic ptw structure"""
     Area = models.ForeignKey(Area, on_delete=models.CASCADE)
-    
+
 
     class Meta:
+        verbose_name = 'Permit to work'
         verbose_name_plural = 'Permits to work'
 
     def __str__(self):
